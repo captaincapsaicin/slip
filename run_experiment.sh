fc22a9488a92ebec8eec0b36c29285c35cabac13
Neil Thomas <thomas.a.neil@gmail.com>
2022-02-11 10:05:13 -0800

    Use source activate

--- run_experiment.sh
+++ run_experiment.sh
@@ -22,5 +22,5 @@
 #SBATCH --mail-user=nthomas@berkeley.edu
 ## Command(s) to run:
 module load ml/tensorflow/2.5.0-py37
-source env activate slip
+source activate slip
 python run_regression_main.py $1
