64896f4faf2768a309f40cb27a8c1275ad7ddb87
Neil Thomas <thomas.a.neil@gmail.com>
2022-05-21 18:23:45 -0700

    Indentation errors. Finally got autoformatter working

--- tuning_test.py
+++ tuning_test.py
@@ -26,74 +26,74 @@ import sampling
 
 class TuningParamsTest(parameterized.TestCase):
 
-  def _get_params(self, seed):
-    """Weight matrix and field vector."""
-    rng = np.random.default_rng(seed)
-    weight_matrix = rng.normal(size=(4, 4, 20, 20))
-    # make symmetric
-    weight_matrix = weight_matrix + np.moveaxis(weight_matrix, (0, 1, 2, 3), (1, 0, 3, 2))
-    field_vec = rng.normal(size=(4, 20))
-    return weight_matrix, field_vec
+    def _get_params(self, seed):
+        """Weight matrix and field vector."""
+        rng = np.random.default_rng(seed)
+        weight_matrix = rng.normal(size=(4, 4, 20, 20))
+        # make symmetric
+        weight_matrix = weight_matrix + np.moveaxis(weight_matrix, (0, 1, 2, 3), (1, 0, 3, 2))
+        field_vec = rng.normal(size=(4, 20))
+        return weight_matrix, field_vec
 
-  def _get_landscape(self, seed, wt_seq=[0, 0, 0, 0], **kwargs):
-    """Return a small PottsModel landscape."""
-    weight_matrix, field_vec = self._get_params(seed)
-    return potts_model.PottsModel(weight_matrix, field_vec, wt_seq=wt_seq, **kwargs)
+    def _get_landscape(self, seed, wt_seq=[0, 0, 0, 0], **kwargs):
+        """Return a small PottsModel landscape."""
+        weight_matrix, field_vec = self._get_params(seed)
+        return potts_model.PottsModel(weight_matrix, field_vec, wt_seq=wt_seq, **kwargs)
 
-  @parameterized.named_parameters(
-      dict(
-          testcase_name='wt_0',
-          seed=0,
-          wt_seq=[0, 0, 0, 0],
-      ),
-      dict(
-          testcase_name='wt_1',
-          seed=1,
-          wt_seq=[1, 1, 1, 1],
-      ),
-  )
-  def test_normalize_to_singles(self, wt_seq, seed):
-    untuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed)
-    tuning_kwargs = tuning.get_tuning_parameters(untuned_landscape, normalize_to_singles=True)
-    tuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed, **tuning_kwargs)
+    @parameterized.named_parameters(
+        dict(
+            testcase_name='wt_0',
+            seed=0,
+            wt_seq=[0, 0, 0, 0],
+        ),
+        dict(
+            testcase_name='wt_1',
+            seed=1,
+            wt_seq=[1, 1, 1, 1],
+        ),
+    )
+    def test_normalize_to_singles(self, wt_seq, seed):
+        untuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed)
+        tuning_kwargs = tuning.get_tuning_kwargs(untuned_landscape, normalize_to_singles=True)
+        tuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed, **tuning_kwargs)
 
-    all_single_fitness = tuned_landscape.evaluate(
-        sampling.get_all_single_mutants(wt_seq, tuned_landscape.vocab_size))
-    np.testing.assert_equal(np.std(all_single_fitness), 1.0)
+        all_single_fitness = tuned_landscape.evaluate(
+            sampling.get_all_single_mutants(wt_seq, tuned_landscape.vocab_size))
+        np.testing.assert_equal(np.std(all_single_fitness), 1.0)
 
-  @parameterized.named_parameters(
-      dict(
-          testcase_name='tuning_2',
-          seed=2,
-          wt_seq=[0, 0, 0, 0],
-          desired_stats_dict={'fraction_adaptive_singles': 0.1,
-                              'fraction_reciprocal_adaptive_epistasis': 0.4,
-                              'epistatic_horizon': 10}
-      ),
-      dict(
-          testcase_name='tuning_1',
-          seed=3,
-          wt_seq=[1, 1, 1, 1],
-          desired_stats_dict={'fraction_adaptive_singles': 0.15,
-                              'fraction_reciprocal_adaptive_epistasis': 0.52,
-                              'epistatic_horizon': 20},
-      ),
-  )
-  def test_tuned_stats(self, wt_seq, seed, desired_stats_dict):
-    untuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed)
+    @parameterized.named_parameters(
+        dict(
+            testcase_name='tuning_2',
+            seed=2,
+            wt_seq=[0, 0, 0, 0],
+            desired_stats_dict={'fraction_adaptive_singles': 0.1,
+                                'fraction_reciprocal_adaptive_epistasis': 0.4,
+                                'epistatic_horizon': 10}
+        ),
+        dict(
+            testcase_name='tuning_1',
+            seed=3,
+            wt_seq=[1, 1, 1, 1],
+            desired_stats_dict={'fraction_adaptive_singles': 0.15,
+                                'fraction_reciprocal_adaptive_epistasis': 0.52,
+                                'epistatic_horizon': 20},
+        ),
+    )
+    def test_tuned_stats(self, wt_seq, seed, desired_stats_dict):
+        untuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed)
 
-    tuning_kwargs = tuning.get_tuning_parameters(
-      untuned_landscape,
-      normalize_to_singles=True,
-      fraction_adaptive_singles=desired_stats_dict['fraction_adaptive_singles'],
-      fraction_reciprocal_adaptive_epistasis=desired_stats_dict['fraction_reciprocal_adaptive_epistasis'],
-      epistatic_horizon=desired_stats_dict['epistatic_horizon'])
+        tuning_kwargs = tuning.get_tuning_kwargs(
+            untuned_landscape,
+            normalize_to_singles=True,
+            fraction_adaptive_singles=desired_stats_dict['fraction_adaptive_singles'],
+            fraction_reciprocal_adaptive_epistasis=desired_stats_dict['fraction_reciprocal_adaptive_epistasis'],
+            epistatic_horizon=desired_stats_dict['epistatic_horizon'])
 
-    tuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed, **tuning_kwargs)
-    actual_stats_dict = tuning.get_landscape_stats(tuned_landscape)
-    for stat in desired_stats_dict.keys():
-      self.assertEqual(desired_stats_dict[stat], actual_stats_dict[stat])
+        tuned_landscape = self._get_landscape(wt_seq=wt_seq, seed=seed, **tuning_kwargs)
+        actual_stats_dict = tuning.get_landscape_stats(tuned_landscape)
+        for stat in desired_stats_dict.keys():
+            self.assertEqual(desired_stats_dict[stat], actual_stats_dict[stat])
 
 
 if __name__ == '__main__':
-  absltest.main()
+    absltest.main()
